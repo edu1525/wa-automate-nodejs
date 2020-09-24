fdd543a35484d6909ea189a06668aad8442b562e
Eduardo de Oliveira Freitas <eduardo.freitas@app4point.com.br>
2020-09-23 23:06:37 -0300

    removed dist

--- dist/api/model/product.d.ts
+++
@@ -1,7 +0,0 @@
-export interface CustomProduct {
-    name: string;
-    description: string;
-    priceAmount1000: number;
-    currency: string;
-    url?: string;
-}
--- dist/controllers/popup/index.d.ts
+++
@@ -1,3 +0,0 @@
-import { ConfigObject } from "../../api/model";
-export declare function popup(config: ConfigObject): Promise<unknown>;
-export declare const closeHttp: () => Promise<void>;
